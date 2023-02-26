023f90b9b3feb000ffef1bd5302cd3a2cff57f81
cliente <cliente@Miros>
2023-02-26 08:25:52 -0300

    alteração

--- programa8/src/programa8/pessoa.java
+++ programa8/src/programa8/pessoa.java
@@ -4,6 +4,13 @@ public class pessoa {
 
 private String nome;
 private String sobrenome;
+private int idade;
+public int getIdade() {
+	return idade;
+}
+public void setIdade(int idade) {
+	this.idade = idade;
+}
 public String getNome() {
 	return nome;
 }
